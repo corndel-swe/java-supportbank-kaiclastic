9d447fce303a86bc06e09b879f0b43ea5ba99ac2
kaiclastic <halajash567@outlook.com>
2024-09-30 11:09:17 +0100

    completed exc 3 passing the tests

--- src/main/java/com/corndel/supportbank/exercises/HelloWorld.java
+++ src/main/java/com/corndel/supportbank/exercises/HelloWorld.java
@@ -5,13 +5,17 @@ import picocli.CommandLine.Command;
 import picocli.CommandLine.Parameters;
 @Command(name = "hello", description = "A CLI printing Hello")
 public class HelloWorld implements Runnable{
+    @Parameters(index = "0",defaultValue = "World")
+    private String name;
     @Override
     public void run(){
-        System.out.println("Hello, World!");
+        String msg = String.format("Hello, %s!",name);
+        System.out.println(msg);
     }
     public static void main(String[]args){
         CommandLine cli = new CommandLine(new HelloWorld());
         int exitCode = cli.execute(args);
         System.exit(exitCode);
+
     }
 }
