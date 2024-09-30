b257c5576cd63dd950044f44c46a7c69babf4874
kaiclastic <halajash567@outlook.com>
2024-09-30 10:54:52 +0100

    passed the test for excercise 2

--- src/main/java/com/corndel/supportbank/exercises/HelloWorld.java
+++ src/main/java/com/corndel/supportbank/exercises/HelloWorld.java
@@ -3,6 +3,15 @@ package com.corndel.supportbank.exercises;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Parameters;
-
-public class HelloWorld {
+@Command(name = "hello", description = "A CLI printing Hello")
+public class HelloWorld implements Runnable{
+    @Override
+    public void run(){
+        System.out.println("Hello, World!");
+    }
+    public static void main(String[]args){
+        CommandLine cli = new CommandLine(new HelloWorld());
+        int exitCode = cli.execute(args);
+        System.exit(exitCode);
+    }
 }
