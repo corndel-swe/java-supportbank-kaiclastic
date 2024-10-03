4e555632a589d85ec0016d483abe1f1f8b9d1d4d
kaiclastic <halajash567@outlook.com>
2024-10-03 11:29:59 +0100

    finished all the exercises

--- src/main/java/com/corndel/supportbank/exercises/PokeAPI.java
+++ src/main/java/com/corndel/supportbank/exercises/PokeAPI.java
@@ -1,8 +1,8 @@
 package com.corndel.supportbank.exercises;
 
-// import kong.unirest.Unirest;
+import kong.unirest.Unirest;
 
-// import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 /**
@@ -27,15 +27,22 @@ public class PokeAPI {
    */
   public static Pokemon getPokemonByName(String name) throws Exception {
     // TODO: Create the url by appending the name to the base url
+    String url = "https://pokeapi.co/api/v2/pokemon/" + name;
 
     // TODO: Make a GET request to the url
     // Hint: Use Unirest.get()
+    var response = Unirest
+            .get(url)
+            .header("Accept", "application/json")
+            .asString();
+
+    String json = response.getBody();
 
     // TODO: Parse the response body into a Pokemon object
     // Hint: Use Jackson's ObjectMapper to map the response body to Pokemon.class
+    ObjectMapper mapper = new ObjectMapper();
+  return mapper.readValue(json, Pokemon.class);
 
-    // TODO: Return the Pokemon
-    return null;
   }
 
   /**
--- src/main/java/com/corndel/supportbank/exercises/Postman.java
+++ src/main/java/com/corndel/supportbank/exercises/Postman.java
@@ -1,6 +1,6 @@
 package com.corndel.supportbank.exercises;
 
-// import kong.unirest.Unirest;
+import kong.unirest.Unirest;
 
 /**
  * This class represents a Message to be sent to the Postman Echo API.
@@ -28,12 +28,18 @@ public class Postman {
    */
   public static String echoMessage(int id, String content) {
     // TODO: Create a Message object with the given id and content
-
+    Message message = new Message(id,content);
+    String url = "https://postman-echo.com/post";
     // TODO: Post the Message object to the Postman Echo API
     // Hint: Use Unirest.post()
+    var response = Unirest.post(url)
+            .header("Content-Type", "application/json")
+            .body(message)
+            .asString();
+
 
     // TODO: Return the response body as a string of JSON
-    return null;
+    return response.getBody();
   }
 
   /**
